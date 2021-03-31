7cd9edf2eeb4812b4e5f03badc332a61844e9436
Viggo <viggo.lagerstedtekholm@gmail.com>
2021-03-31 18:57:43 +0200

    Added response and request objects.

---
+++ src/main/java/com/mibsystem/demo/Models/Response/MessageObject.java
@@ -0,0 +1,17 @@
+package com.mibsystem.demo.Models.Response;
+
+public class MessageObject {
+    private String message;
+
+    public MessageObject(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
